63bf264f72b538bf44ed53331c72c3e5c8607091
Josh Friedman <josh.nmr@gmail.com>
2016-06-07 23:05:33 -0400

    more fancy stuff

--- app.py
+++ app.py
@@ -9,13 +9,20 @@ app = Flask(__name__)
 gMapsKey = "AIzaSyBJ0-ZPQengxtnHbCu1KotNA_yqyI_OD4Q"
 callStr = "https://maps.googleapis.com/maps/api/js?callback=initMap&key="
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/', methods=['GET','POST'])
 def index():
 
 	if request.method == 'GET':
-		return render_template('main.html')
+		return render_template('index.html')
 
-	
+	else:
+		loc1, loc2   =  request.form['loc1'], request.form['loc2']
+		querry       =  request.form['querry']
+		transit_mode =  request.form['transit_mode']
+
+		print loc1, loc2, querry, transit_mode
+
+		return render_template('index.html')
 
 if __name__ == "__main__":
 	app.run(debug=True)
